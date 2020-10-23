2e1dfdda57c737af820c13b098a52662637d6540
samarth2901 <66294456+samarth2901@users.noreply.github.com>
2020-10-23 11:13:32 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+public long add(long first, long second) {
+    return first + second;
+}
+
+public long subtract(long first, long subtract) {
+    return first - second;
+}
+
+public long multiply(long first, long subtract) {
+    return first * second;
+}
+
+public long division(long first, long second) {
+     return first/second;
+}
+}
